e363139c78858b55a7f52810e2717a6ac9ce0a1e
EleazarFabrizio <ezrfabrizio@gmail.com>
2023-06-12 11:58:36 -0300

    carrito

--- Carrito.py
+++ Carrito.py
@@ -1,4 +1,22 @@
 
 ## PRE-ALPHA 0.0.1
 
-numero = 0
+carrito = {
+
+"total" : 0 ,
+
+}
+
+print(f"""
+Bienvnido al carrito mas fachero del mundo mundial.
+
+{carrito}
+
+1) Mostrar productos en detalle:
+2) Mostrar información breve del producto:
+3) Buscar producto por código:
+4) Realizar compra:
+5) Editar carrito:
+6) Finalizar compra:
+7) Salir:
+""")
