944eb097b3794e368c7b29a7709bd8fb6863c458
EleazarFabrizio <ezrfabrizio@gmail.com>
2023-09-04 08:32:26 -0300

    me olvide de agregar stock xd

--- base de datos/programa.py
+++ base de datos/programa.py
@@ -11,7 +11,7 @@ conexion = mysql.connector.connect(host="localhost",user="root",password="",data
 def cargar_datos():
     tree.delete(*tree.get_children()) # Borrar datos existentes en el Treeview
     cursor = conexion.cursor()
-    cursor.execute("select producto.nombre, producto.precio, marca.nombre, categoria.nombre from producto join marca on marca.codmarca = producto.codmarca join categoria on categoria.codcategoria = producto.codcategoria;")
+    cursor.execute("select producto.nombre, producto.precio, marca.nombre, categoria.nombre, producto.stock from producto join marca on marca.codmarca = producto.codmarca join categoria on categoria.codcategoria = producto.codcategoria")
     for row in cursor.fetchall():
         tree.insert("", "end", values=row)
 
@@ -20,16 +20,19 @@ root = Tk()
 root.title("Consultar base de datos de almacen fantabuloso :P")
 
 # Crear Treeview para mostrar la información
-tree = ttk.Treeview(root, columns=("Nombre", "Precio", "Marca","Categoria"))
+tree = ttk.Treeview(root, columns=("Nombre", "Precio", "Stock", "Marca","Categoria"))
 tree.heading("#1", text="Nombre")
 tree.heading("#2", text="Precio")
-tree.heading("#3", text="Marca")
-tree.heading("#4", text="Categoria")
+tree.heading("#3", text="Stock")
+tree.heading("#4", text="Marca")
+tree.heading("#5", text="Categoria")
+
 tree.column("#0", width=0, stretch=NO)
 tree.column("Nombre", anchor=CENTER)
 tree.column("Precio", anchor=CENTER)
 tree.column("Marca", anchor=CENTER)
 tree.column("Categoria", anchor=CENTER)
+tree.column("Stock", anchor=CENTER)
 tree.pack(padx=10, pady=10)
 
 
